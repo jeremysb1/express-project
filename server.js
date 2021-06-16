21a366a3732bf599a3bb783b4b8e1a4aed583390
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-06-16 10:35:00 -0400

    adding routes

--- server.js
+++ server.js
@@ -4,11 +4,31 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send({
-  	id: 1,
+const friends = [
+  {
+  	id: 0,
   	name: 'Ol Jimbo'
-  });
+  },
+  {
+  	id: 1,
+  	name: 'Stuntman Mike'
+  }
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+  	res.status(200).json(friend);
+  } else {
+  	res.status(404).json({
+  	  error: "Friend does not exist"
+  	});
+  }
 });
 
 app.get('/messages', (req, res) => {
