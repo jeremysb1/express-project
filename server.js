d9e4c369e6edf8fb3cf0c5eed19a8dfedb6869bd
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-06-16 11:33:13 -0400

    making post requests in express

--- server.js
+++ server.js
@@ -22,6 +22,24 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) {
+  	res.status(400).json({
+  	  error: 'Missing friend name'
+  	});
+  }
+
+  const newFriend = {
+  	name: req.body.name,
+  	id: friends.length
+  };
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
+
 app.get('/friends', (req, res) => {
   res.json(friends);
 });
@@ -33,7 +51,7 @@ app.get('/friends/:friendId', (req, res) => {
   	res.status(200).json(friend);
   } else {
   	res.status(404).json({
-  	  error: "Friend does not exist"
+  	  error: 'Friend does not exist'
   	});
   }
 });
